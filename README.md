58f81a9158f8634f0e45d8cdb165a88f3e5fd340
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-05 23:02:08 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# restaurant
+# Restaurant
+
+__This restaurant app creates a burger logger with MySQL, Node.js, Express.js, Handlebars.js and an ORM. It uses Node and MySQL to query and route data to the app, and Handlebars to generate HTML.__
+
+
+* This app allows users to input the names of burgers they'd like to eat.
+* Whenever a user submits a burger's name, the app places the burger on the left side of the page -- the waiting room.
+* Each burger in the waiting area has a Devour it! button. When the user clicks it, the burger has been devoured and moves to the right side of the page.
+* The app stores every burger in a database, whether devoured or not.
---
+++ config/orm.js
@@ -0,0 +1,52 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+
+  selectAll: function(tableName, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableName], function(err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+
+  insertOne: function(tableName, colName, value, cb) {
+    var queryString = "INSERT INTO ?? SET ?? = ?";
+
+    console.log(queryString);
+
+    connection.query(queryString, [tableName, colName, value], function(err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
+  }
+
+  // updateOne: function(tableName, colName, newValue, whereColName, value, cb) {
+  //   var queryString = "UPDATE " + tableName + "  SET ?? WHERE ??";
+
+  //   console.log(queryString);
+
+  //   connection.query(queryString, 
+  //     [
+  //       {
+  //         colName: newValue
+  //       },
+  //       {
+  //         id: value.id
+  //       }
+  //     ], 
+      
+  //     function(err, result) {
+  //       if (err) throw err;
+
+  //     cb(result);
+  //   });
+  // }
+
+};
+
+
+module.exports = orm;
