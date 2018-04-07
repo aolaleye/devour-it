b54581a45bd6aba5a0625e595e6f52ded8d50542
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-07 01:33:29 -0400

    fixed orm commands

--- config/orm.js
+++ config/orm.js
@@ -1,52 +1,39 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 var orm = {
 
-  selectAll: function(tableName, cb) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableName], function(err, result) {
+  selectAll: function (cb) {
+    connection.query("SELECT * FROM burgers", function (err, result) {
       if (err) throw err;
-
       cb(result);
     });
   },
 
-  insertOne: function(tableName, colName, value, cb) {
-    var queryString = "INSERT INTO ?? SET ?? = ?";
-
-    console.log(queryString);
-
-    connection.query(queryString, [tableName, colName, value], function(err, result) {
-      if (err) throw err;
-
-      cb(result);
-    });
-  },
-
-  updateOne: function(tableName, colName, newValue, condition, cb) {
-    var queryString = "UPDATE " + tableName + "  SET ?? WHERE ??";
-
-    console.log(queryString);
-
-    connection.query(queryString, 
-      [
-        {
-          colName: newValue
-        },
-        {
-          id: condition
-        }
-      ], 
-      
-      function(err, result) {
+  insertOne: function (burger_name, cb) {
+    connection.query(
+      "INSERT INTO burgers (burger_name) VALUE (?)",
+      burger_name,
+      function (err, result) {
         if (err) throw err;
+        cb(result);
+      }
+    );
+  },
 
-      cb(result);
-    });
+  updateOne: function (id, cb) {
+    connection.query(
+      "UPDATE burgers SET ? WHERE ?", [{
+        devoured: true
+      }, {
+        id: id
+      }],
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
   }
 
 };
-
 
 module.exports = orm;
